d8e47e94de8971b98f8a6e9ab9481b3d297eeb79
junya uchizono <junya19991215@icloud.com>
2023-03-04 13:34:55 +0900

    delete object

--- kernel/.logger.d
+++
@@ -1,8 +0,0 @@
-logger.o: logger.cpp logger.hpp \
-  /Users/uchienuruunari/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
-  /Users/uchienuruunari/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /Users/uchienuruunari/osbook/devenv/x86_64-elf/include/c++/v1/version \
-  /Users/uchienuruunari/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
-  /Users/uchienuruunari/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  console.hpp drawing.hpp frame_buffer_config.hpp \
-  /Users/uchienuruunari/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h
