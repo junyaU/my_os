8b25a7fd617c409dd1a1d35105f342f5ae5d7965
junya uchizono <junya19991215@icloud.com>
2023-04-22 15:25:03 +0900

    fix terminal

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -2,6 +2,7 @@
 
 #include "console.hpp"
 #include "font.hpp"
+#include "layer.hpp"
 
 Terminal::Terminal() {
     window_ = std::make_shared<ToplevelWindow>(
@@ -19,17 +20,76 @@ Rectangle<int> Terminal::BlinkCursor() {
     cursor_visible_ = !cursor_visible_;
     DrawCursor(cursor_visible_);
 
-    return {ToplevelWindow::kTopLeftMargin +
-                Vector2D<int>{4 + kFontHorizonPixels * cursor_pos_.x,
-                              5 + kFontVerticalPixels * cursor_pos_.y},
-            {7, 15}};
+    return {CalcCursorPos(), {7, 15}};
 }
 
 void Terminal::DrawCursor(bool visible) {
     const auto color = visible ? ToColor(0xffffff) : ToColor(0);
-    const auto pos = Vector2D<int>{4 + kFontHorizonPixels * cursor_pos_.x,
-                                   5 + kFontVerticalPixels * cursor_pos_.y};
-    FillRectangle(*window_->InnerDrawer(), pos, {7, 15}, color);
+    FillRectangle(*window_->InnerDrawer(), CalcCursorPos(), {7, 15}, color);
+}
+
+Vector2D<int> Terminal::CalcCursorPos() const {
+    return ToplevelWindow::kTopLeftMargin +
+           Vector2D<int>{4 + kFontHorizonPixels * cursor_pos_.x,
+                         4 + kFontVerticalPixels * cursor_pos_.y};
+}
+
+Rectangle<int> Terminal::InputKey(uint8_t modifier, uint8_t keycode,
+                                  char ascii) {
+    DrawCursor(false);
+
+    Rectangle<int> draw_area{CalcCursorPos(),
+                             {kFontHorizonPixels * 2, kFontVerticalPixels}};
+
+    if (ascii == '\n') {
+        linebuf_[linebuf_index_] = 0;
+        linebuf_index_ = 0;
+        cursor_pos_.x = 0;
+
+        if (cursor_pos_.y < kRows - 1) {
+            ++cursor_pos_.y;
+        } else {
+            Scroll1();
+        }
+
+        draw_area.pos = ToplevelWindow::kTopLeftMargin;
+        draw_area.size = window_->InnerSize();
+    } else if (ascii == '\b' && cursor_pos_.x > 0) {
+        --cursor_pos_.x;
+        FillRectangle(*window_->Drawer(), CalcCursorPos(),
+                      {kFontHorizonPixels, kFontVerticalPixels}, {0, 0, 0});
+
+        draw_area.pos = CalcCursorPos();
+
+        if (linebuf_index_ > 0) {
+            --linebuf_index_;
+        }
+    } else if (ascii != 0 && cursor_pos_.x < kColumns - 1 &&
+               linebuf_index_ < kLineMax - 1) {
+        linebuf_[linebuf_index_] = ascii;
+        ++linebuf_index_;
+
+        WriteAscii(*window_->Drawer(), CalcCursorPos(), ascii, {255, 255, 255});
+        ++cursor_pos_.x;
+    }
+
+    DrawCursor(true);
+
+    return draw_area;
+}
+
+void Terminal::Scroll1() {
+    Rectangle<int> move_src{
+        ToplevelWindow::kTopLeftMargin +
+            Vector2D<int>{4, 4 + kFontVerticalPixels},
+        {kColumns * kFontHorizonPixels, (kRows - 1) * kFontVerticalPixels}};
+
+    window_->Move(ToplevelWindow::kTopLeftMargin + Vector2D<int>{4, 4},
+                  move_src);
+
+    FillRectangle(
+        *window_->InnerDrawer(), {4, 4 + kFontVerticalPixels * cursor_pos_.y},
+        {kFontHorizonPixels * kColumns, kFontVerticalPixels}, {0, 0, 0});
 }
 
 void TaskTerminal(uint64_t task_id, int64_t data) {
@@ -40,6 +100,7 @@ void TaskTerminal(uint64_t task_id, int64_t data) {
     layer_manager->Move(terminal->LayerID(), {100, 200});
     active_layer->Activate(terminal->LayerID());
 
+    layer_task_map->insert(std::make_pair(terminal->LayerID(), task_id));
     __asm__("sti");
 
     while (true) {
@@ -52,19 +113,27 @@ void TaskTerminal(uint64_t task_id, int64_t data) {
         }
 
         switch (msg->type) {
-            case Message::kTimerTimeout:
-                terminal->BlinkCursor();
-                {
-                    const auto area = terminal->BlinkCursor();
-                    Message msg =
-                        MakeLayerMessage(task_id, terminal->LayerID(),
-                                         LayerOperation::DrawArea, area);
+            case Message::kTimerTimeout: {
+                const auto area = terminal->BlinkCursor();
+                Message msg = MakeLayerMessage(task_id, terminal->LayerID(),
+                                               LayerOperation::DrawArea, area);
 
-                    __asm__("cli");
-                    task_manager->SendMessage(1, msg);
-                    __asm__("sti");
-                }
-                break;
+                __asm__("cli");
+                task_manager->SendMessage(1, msg);
+                __asm__("sti");
+            } break;
+            case Message::kKeyPush: {
+                const auto area = terminal->InputKey(msg->arg.keyboard.modifier,
+                                                     msg->arg.keyboard.keycode,
+                                                     msg->arg.keyboard.ascii);
+
+                Message msg = MakeLayerMessage(task_id, terminal->LayerID(),
+                                               LayerOperation::DrawArea, area);
+
+                __asm__("cli");
+                task_manager->SendMessage(1, msg);
+                __asm__("sti");
+            } break;
             default:
                 break;
         }
