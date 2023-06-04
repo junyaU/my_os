fa1c1f606b2502ea153f09ed9638a89dce0d12d9
junya uchizono <junya19991215@icloud.com>
2023-06-05 08:51:08 +0900

    define page_map_entry

--- kernel/paging.hpp
+++ kernel/paging.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 const size_t kPageDirectoryCount = 64;
 
@@ -8,3 +9,32 @@ const size_t kPageDirectoryCount = 64;
 void SetupIdentityPageTable();
 
 void InitializePaging();
+
+union PageMapEntry {
+    uint64_t data;
+
+    struct {
+        uint64_t present : 1;
+        uint64_t writable : 1;
+        uint64_t user : 1;
+        uint64_t write_through : 1;
+        uint64_t cache_disable : 1;
+        uint64_t accessed : 1;
+        uint64_t dirty : 1;
+        uint64_t huge_page : 1;
+        uint64_t global : 1;
+        uint64_t : 3;
+
+        // 　下位の階層を指す物理アドレス
+        uint64_t addr : 40;
+        uint64_t : 12;
+    } __attribute__((packed)) bits;
+
+    PageMapEntry* Pointer() const {
+        return reinterpret_cast<PageMapEntry*>(bits.addr << 12);
+    }
+
+    void SetPointer(PageMapEntry* entry) {
+        bits.addr = reinterpret_cast<uint64_t>(entry) >> 12;
+    }
+};
