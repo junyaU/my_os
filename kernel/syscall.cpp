69674479f65d9da0d47dc0c5c175c816b9a90cce
Junya Uchizono <junya19991215@icloud.com>
2023-06-30 22:25:47 +0900

    add lines

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cerrno>
+#include <cmath>
 #include <cstdint>
 
 #include "asmfunc.h"
@@ -131,16 +132,63 @@ SYSCALL(WinRedraw) {
     return DoWindowFunc([](Window &) { return Result{0, 0}; }, arg1);
 }
 
+SYSCALL(WinDrawLine) {
+    return DoWindowFunc(
+        [](Window &win, int x0, int y0, int x1, int y1, uint32_t color) {
+            auto sign = [](int x) { return (x > 0) ? 1 : (x < 0) ? -1 : 0; };
+
+            const int dx = x1 - x0 + sign(x1 - x0);
+            const int dy = y1 - y0 + sign(y1 - y0);
+
+            if (dx == 0 && dy == 0) {
+                win.Drawer()->Draw({x0, y0}, ToColor(color));
+                return Result{0, 0};
+            }
+
+            const auto floord = static_cast<double (*)(double)>(floor);
+            const auto ceild = static_cast<double (*)(double)>(ceil);
+
+            if (abs(dx) >= abs(dy)) {
+                if (dx < 0) {
+                    std::swap(x0, x1);
+                    std::swap(y0, y1);
+                }
+
+                const auto roundish = y1 >= y0 ? floord : ceild;
+                const double m = static_cast<double>(dy) / dx;
+                for (int x = x0; x <= x1; x++) {
+                    const int y = roundish(m * (x - x0) + y0);
+                    win.Drawer()->Draw({x, y}, ToColor(color));
+                }
+            } else {
+                if (dy < 0) {
+                    std::swap(x0, x1);
+                    std::swap(y0, y1);
+                }
+
+                const auto roundish = x1 >= x0 ? floord : ceild;
+                const double m = static_cast<double>(dx) / dy;
+                for (int y = y0; y <= y1; y++) {
+                    const int x = roundish(m * (y - y0) + x0);
+                    win.Drawer()->Draw({x, y}, ToColor(color));
+                }
+            }
+
+            return Result{0, 0};
+        },
+        arg1, arg2, arg3, arg4, arg5, arg6);
+}
+
 #undef SYSCALL
 }  // namespace syscall
 
 using SyscallFuncType = syscall::Result(uint64_t, uint64_t, uint64_t, uint64_t,
                                         uint64_t, uint64_t);
 
-extern "C" std::array<SyscallFuncType *, 8> syscall_table{
+extern "C" std::array<SyscallFuncType *, 9> syscall_table{
     syscall::LogString,      syscall::PutString,      syscall::Exit,
     syscall::OpenWindow,     syscall::WinWriteString, syscall::WinFillRectangle,
-    syscall::GetCurrentTick, syscall::WinRedraw,
+    syscall::GetCurrentTick, syscall::WinRedraw,      syscall::WinDrawLine,
 };
 
 void InitializeSysCall() {
